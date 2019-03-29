8c2612f28b76031f1e91b13274ab57baf13c95f3
Christian Paul Dehli <dehli@gatech.edu>
2019-03-28 23:36:36 -0400

    Adds additional gateway helpers

--- src/serverless/aws/api_gateway.cljs
+++ src/serverless/aws/api_gateway.cljs
@@ -1,6 +1,7 @@
 (ns serverless.aws.api-gateway
   (:require [goog.object :as gobj]
-            [serverless.aws.sdk :refer [AWS]]))
+            [serverless.aws.sdk :refer [AWS]]
+            [serverless.json :refer [json->clj]]))
 
 (defonce ^:private ManagementApi (gobj/get AWS "ApiGatewayManagementApi"))
 
@@ -8,3 +9,10 @@
   (-> {:apiVersion "2018-11-29" :endpoint endpoint}
       clj->js
       ManagementApi.))
+
+(defonce request-context :requestContext)
+(defonce connection-id (comp :connectionId request-context))
+(defonce authorizer (comp :authorizer request-context))
+(defonce sub (comp :principalId authorizer))
+(defonce body #(-> % :body (json->clj :keywordize-keys true)))
+(defonce endpoint (comp #(str (:domainName %) "/" (:stage %)) request-context))
