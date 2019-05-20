160dba4197bcce483a4696342846a77e1ce6fc6c
Christian Paul Dehli <dehli@gatech.edu>
2019-05-20 08:31:22 -0400

    0.1.3 release
    Adds ses and cognito helpers

---
+++ src/serverless/aws/user_pools.cljs
@@ -0,0 +1,21 @@
+(ns aws.serverless.user-pools
+  (:require [goog.object :as gobj]
+            [serverless.aws.sdk :refer [AWS js-call]]))
+
+(defonce ^:private CognitoIdentityServiceProvider
+  (gobj/get AWS "CognitoIdentityServiceProvider"))
+
+(defonce ^:private service-provider ^:private
+  (CognitoIdentityServiceProvider.))
+
+(defonce admin-create-user
+  (partial js-call service-provider "adminCreateUser"))
+
+(defonce admin-get-user
+  (partial js-call service-provider "adminGetUser"))
+
+(defonce confirm-sign-up
+  (partial js-call service-provider "confirmSignUp"))
+
+(defonce list-users
+  (partial js-call service-provider "listUsers"))
