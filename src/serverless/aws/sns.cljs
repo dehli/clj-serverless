9c5fa7b8f472c6382b0912b0648d26292ed04802
Christian Paul Dehli <dehli@gatech.edu>
2020-01-23 06:29:59 -0500

    Adds sns namespace

---
+++ src/serverless/aws/sns.cljs
@@ -0,0 +1,21 @@
+(ns serverless.aws.sns
+  (:require [goog.object :as gobj]
+            [serverless.aws.sdk :refer [AWS js-call-v2]]
+            [serverless.core :refer [def-]]
+            [serverless.json :refer [clj->json]]))
+
+(def- SNS (gobj/get AWS "SNS"))
+
+;; Constructor
+(defn service
+  [^String topic-arn]
+  (new SNS (clj->js {:apiVersion "2010-03-31"
+                     :params {:TopicArn topic-arn}})))
+
+;; Method helpers
+(defn- base-xform [{:keys [message]}]
+  {:message (clj->json message)})
+
+;; Public methods
+(def publish
+  (partial js-call-v2 base-xform))
