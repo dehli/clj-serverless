50449199aee34c3208467a9c52dd51bcd25cbb03
Christian Paul Dehli <dehli@gatech.edu>
2020-01-23 20:43:36 -0500

    Simplifies sns publish

--- src/serverless/aws/sns.cljs
+++ src/serverless/aws/sns.cljs
@@ -1,8 +1,7 @@
 (ns serverless.aws.sns
   (:require [goog.object :as gobj]
             [serverless.aws.sdk :refer [AWS js-call-v2]]
-            [serverless.core :refer [def-]]
-            [serverless.json :refer [clj->json]]))
+            [serverless.core :refer [def-]]))
 
 (def- SNS (gobj/get AWS "SNS"))
 
@@ -12,10 +11,6 @@
   (new SNS (clj->js {:apiVersion "2010-03-31"
                      :params {:TopicArn topic-arn}})))
 
-;; Method helpers
-(defn- base-xform [message]
-  {:message (clj->json message)})
-
 ;; Public methods
 (def publish
-  (partial js-call-v2 "publish" base-xform))
+  (partial js-call-v2 "publish" identity))
