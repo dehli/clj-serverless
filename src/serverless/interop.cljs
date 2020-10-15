54770bb90b05b21e6b3866d14609968802c1901e
Christian Paul Dehli <github@deh.li>
2020-10-15 18:29:21 -0400

    Adds promise-fn->chan method

--- src/serverless/interop.cljs
+++ src/serverless/interop.cljs
@@ -1,7 +1,7 @@
 (ns serverless.interop
   (:require [camel-snake-kebab.core :as csk]
             [camel-snake-kebab.extras :as cske]
-            [cljs-bean.core :refer [->clj]]
+            [cljs-bean.core :refer [->clj ->js]]
             [cljs.core.async.interop :refer-macros [<p!]]
             [serverless.core.async :refer [go-try]]))
 
@@ -9,7 +9,15 @@
   [obj]
   (cske/transform-keys csk/->kebab-case (->clj obj)))
 
+(defn ->jsCamelCase
+  [obj]
+  (->js (cske/transform-keys csk/->camelCase obj)))
+
 (defn promise->chan
   [promise]
   (go-try
     (->clj-kebab-case (<p! promise))))
+
+(defn promise-fn->chan
+  [promise-fn arguments]
+  (-> arguments ->jsCamelCase promise-fn promise->chan))
