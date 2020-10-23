34eb3bd166164efd5b1714df65bd687aacf8a600
Christian Paul Dehli <github@deh.li>
2020-10-23 08:25:46 -0400

    Adds transit instant test

--- src/serverless/transit.cljs
+++ src/serverless/transit.cljs
@@ -41,8 +41,10 @@
 (def ^:private default-reader (t/reader :json {:handlers read-handlers}))
 (def ^:private default-writer (t/writer :json {:handlers write-handlers}))
 
-(defn read [x]
-  (t/read default-reader x))
+(defn read
+  ([x] (read default-reader x))
+  ([reader x] (t/read reader x)))
 
-(defn write [x]
-  (t/write default-writer x))
+(defn write
+  ([x] (write default-writer x))
+  ([writer x] (t/write writer x)))
