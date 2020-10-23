d259500e07452bef3a2c5f89557e0f9aab9c0523
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2020-10-23 08:15:31 -0400

    Adds time readers / writers

--- src/serverless/transit.cljs
+++ src/serverless/transit.cljs
@@ -1,9 +1,45 @@
 (ns serverless.transit
-  (:require [cognitect.transit :as t])
+  (:require [cognitect.transit :as t]
+            [time-literals.read-write]
+            [java.time :refer [Period
+                               LocalDate
+                               LocalDateTime
+                               ZonedDateTime
+                               Instant
+                               ZoneId
+                               DayOfWeek
+                               LocalTime
+                               Month
+                               Duration
+                               Year
+                               YearMonth]])
   (:refer-clojure :exclude [read]))
 
-(def ^:private default-reader (t/reader :json))
-(def ^:private default-writer (t/writer :json))
+(def time-classes
+  {'period Period
+   'date LocalDate
+   'date-time LocalDateTime
+   'zoned-date-time ZonedDateTime
+   'instant Instant
+   'time LocalTime
+   'duration Duration
+   'year Year
+   'year-month YearMonth
+   'zone ZoneId
+   'day-of-week DayOfWeek
+   'month Month})
+
+(def write-handlers
+  (into {}
+    (for [[tick-class host-class] time-classes]
+      [host-class (t/write-handler (constantly (name tick-class)) str)])))
+
+(def read-handlers
+  (into {} (for [[sym fun] time-literals.read-write/tags]
+             [(name sym) (t/read-handler fun)])))
+
+(def ^:private default-reader (t/reader :json {:handlers read-handlers}))
+(def ^:private default-writer (t/writer :json {:handlers write-handlers}))
 
 (defn read [x]
   (t/read default-reader x))
